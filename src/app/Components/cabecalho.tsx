70c564445593fdafba24b35589b6aeb74ccc49c3
Nick1em <nicolekanandadeoc@gmail.com>
2025-04-28 18:08:07 -0300

    - Feito todo o css da parte de cabeçalho e navegação. ( A font será alterada posteriormente) -Ajustado corretamente o caminho de <img>

--- src/app/Components/cabecalho.tsx
+++ src/app/Components/cabecalho.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
+import styles from "../style/nav.module.css"
+import Image from "next/image";
+import { link } from "fs";
 
 export default function Cabecalho (){
    return (
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | {''}
-        <Link href="/Produtos">Produtos</Link> | {''}
-        <Link href="/Calculadora">Calculadora</Link> | {''}
-      </nav>
+    <header className={styles.header}>
+      <div className= {styles.cabecalho}>
+        <div>
+          <img className={styles.logo}  src="/img/arvore branca sf.png" alt="Logo Móveis Gondor" />
+        </div>
+        <nav className={styles.nav}>
+          <Link href="/">Home</Link> | {''}
+          <Link href="/Produtos">Produtos</Link> | {''}
+          <Link href="/Calculadora">Calculadora</Link> 
+        </nav>
+      </div>
     </header>
    )
 }
